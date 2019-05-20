060167e38e95b9d02ea19a66e30cb086e74b0221
shathee <sat.sust@gmail.com>
2019-05-21 01:55:39 +0600

    logout redirection changed

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -13,28 +13,34 @@
 	</div>
     <div class="row home-page-class">
     	
-    	<div class="card col-md-3 text-center">
+    	<div class="card col-md-6 offset-md-3 text-center">
     		<a href="{{ url('search')}}">	
 			  <div class="card-body bg-dark rounded">
-			    <h5 class="card-title h-button">Bidders Certificate Management System</h5>
+			    <h5 class="card-title h-button">Contract Information System</h5>
 			  </div>
 		  	</a>
 		</div>
-    	<div class="card col-md-3 text-center">
+		    	
+    	</div>
+		
+    	<div class="row">&nbsp;</div>
+
+    	<div class="row home-page-class">
+    	<div class="card col-md-4 text-center">
     		<a href="{{ url('rules')}}">	
 			  <div class="card-body bg-dark rounded">
 			    <h5 class="card-title h-button">Act, Rules & Guidelines</h5>
 			  </div>
 		  	</a>
 		</div>
-    	<div class="card col-md-3 text-center">
+    	<div class="card col-md-4 text-center">
     		<a href="{{ url('search')}}">	
 			  <div class="card-body bg-dark rounded">
 			    <h5 class="card-title h-button">Delegation of Financial Power</h5>			    
 			  </div>
 		  	</a>
 		</div>
-    	<div class="card col-md-3 text-center">
+    	<div class="card col-md-4 text-center">
     		<a href="{{ url('search')}}">	
 			  <div class="card-body bg-dark rounded">
 			    <h5 class="card-title h-button align-middle">Circulars/Office Orders</h5>
@@ -45,6 +51,8 @@
 		
     	
     	</div>
+
+
     	<div class="row">
 		<div class="col-md-10 offset-md-1 text-center">
 	      &nbsp;<br>
